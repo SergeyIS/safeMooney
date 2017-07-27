eb4cc01bce9a0ad86374cb9c7d615c3c0cc49ad3
Sergey <sergey.ivanovich.sidorov@gmail.com>
2017-07-27 23:08:59 +0300

    created a couple of activities and menu

---
+++ app/src/main/java/com/safemooney/Transaction.java
@@ -0,0 +1,52 @@
+package com.safemooney;
+
+/**
+ * Created by root on 7/27/17.
+ */
+
+public class Transaction {
+
+    private String username;
+    private String count;
+    private byte[] image;
+    private  boolean isMe;
+
+    public Transaction(String username, String count, byte[] image, boolean isMe){
+        this.username = username;
+        this.count = count;
+        this.image = image;
+        this.isMe = isMe;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public boolean getIsMe() {
+        return isMe;
+    }
+
+    public void setIsMe(boolean me) {
+        isMe = me;
+    }
+
+    public byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
+
+    public String getCount() {
+        return count;
+    }
+
+    public void setCount(String count) {
+        this.count = count;
+    }
+}
