c1466106b7405d43c219373e151c2c17f41a068f
sergey <sergey.ivanovich.sidorov@gmail.com>
2017-08-12 16:43:36 +0300

    add gson library, AccountClient and TransactionClient i com.safemooney.http package to work with web service

--- app/src/main/java/com/safemooney/app/LoginActivity.java
+++ app/src/main/java/com/safemooney/app/LoginActivity.java
@@ -1,45 +1,116 @@
 package com.safemooney.app;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.safemooney.http.models.*;
+import com.safemooney.http.*;
 import com.safemooney.R;
 
-public class LoginActivity extends AppCompatActivity {
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
+import java.util.Scanner;
+
+public class LoginActivity extends AppCompatActivity
+{
 
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_login);
 
-        TextView loginView = (TextView) findViewById(R.id.loin_edit);
-        TextView passwordView = (TextView) findViewById(R.id.password_edit);
         Button signinBtn = (Button) findViewById(R.id.signin_btn);
+        signinBtn.setOnClickListener(new ButtonClickListener(this));
 
-        final Intent mainIntent = new Intent();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        SharedPreferences preferences = getSharedPreferences("userdata", MODE_PRIVATE);
+        int userId = preferences.getInt("userId", -1);
+
+        if(userId < 0)
+            return;
+
+        Intent mainIntent = new Intent();
         mainIntent.setClass(this, MainActivity.class);
+        startActivity(mainIntent);
+        finish();
+    }
 
-        signinBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+    private class ButtonClickListener implements View.OnClickListener
+    {
+        private Context context;
 
-                try{
-                    startActivity(mainIntent);
-                    finish();
+        public ButtonClickListener(Context context)
+        {
+            this.context = context;
+        }
+
+        @Override
+        public void onClick(View view)
+        {
+            try
+            {
+                final AccountClient accountClient = new AccountClient();
+
+                final User[] currentUser = {null};
+
+                final String username = ((EditText) findViewById(R.id.loin_edit)).getText().toString();
+                final String password = ((EditText) findViewById(R.id.password_edit)).getText().toString();
+
+                Thread accountThread = new Thread(new Runnable() {
+                    @Override
+                    public void run()
+                    {
+                        currentUser[0] = accountClient.logIn(username, password);
+                    }
+                });
+                accountThread.start();
+                accountThread.join();
+
+                if(currentUser[0] == null)
+                {
+                    Toast toast = Toast.makeText(context, "This user is not found",Toast.LENGTH_LONG);
+                    toast.show();
+                    return;
                 }
-                catch (Exception e){
-                    Log.d("mytag", "", e);
-                }
+
+                SharedPreferences.Editor preferencesEditor = context.getSharedPreferences("userdata", MODE_PRIVATE).edit();
+                preferencesEditor.putInt("userId", currentUser[0].getId());
+                preferencesEditor.putString("username", currentUser[0].getUsername());
+                preferencesEditor.putString("firstname", currentUser[0].getFirstname());
+                preferencesEditor.putString("lastname", currentUser[0].getLastname());
+                preferencesEditor.putString("tokenkey", currentUser[0].getTokenkey());
+                preferencesEditor.apply();
+
+                Intent mainIntent = new Intent();
+                mainIntent.setClass(context, MainActivity.class);
+                context.startActivity(mainIntent);
+                finish();
             }
-        });
-
+            catch (Exception e)
+            {
+                Log.d("mytag", e.toString());
+            }
+        }
     }
 }
--- app/src/main/java/com/safemooney/app/MainActivity.java
+++ app/src/main/java/com/safemooney/app/MainActivity.java
@@ -1,8 +1,10 @@
 package com.safemooney.app;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -10,15 +12,18 @@ import android.view.View;
 import android.widget.ListView;
 
 import com.safemooney.R;
+import com.safemooney.http.AccountClient;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
     ArrayList<Transaction> transactionList;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -34,38 +39,80 @@ public class MainActivity extends AppCompatActivity {
 
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
 
         getMenuInflater().inflate(R.menu.main_menu, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
 
         switch (item.getItemId()){
 
             case (R.id.addnew_bar):
                 addNewTransaction();
                 break;
+            case (R.id.logout_bar):
+                logOut();
+                break;
         }
         return true;
     }
 
-    private void addNewTransaction(){
+    private void addNewTransaction()
+    {
 
         Intent addTransIntent = new Intent();
         addTransIntent.setClass(this, AddTransactionActivity.class);
 
-        try{
+        try
+        {
             startActivity(addTransIntent);
         }
-        catch (Exception e){
+        catch (Exception e)
+        {
 
         }
 
     }
-    private void InitializeTransactionList(){
+    private void logOut()
+    {
+        SharedPreferences preferences = getSharedPreferences("userdata", MODE_PRIVATE);
+
+        final int userId = preferences.getInt("userId", -1);
+        final String tokenkey = preferences.getString("tokenkey", null);
+
+        Thread logoutThread = new Thread(new Runnable() {
+            @Override
+            public void run()
+            {
+                AccountClient accountClient = new AccountClient();
+                try
+                {
+                    boolean result = accountClient.logOut(userId, tokenkey);
+
+                    Log.d("mytag", "result of logout " + Boolean.toString(result));
+                }
+                catch(Exception e)
+                {
+                    Log.d("mytag", e.toString());
+                }
+
+            }
+        });
+        logoutThread.start();
+        preferences.edit().putInt("userId", -1).apply();
+
+        Intent loginIntent = new Intent();
+        loginIntent.setClass(this, LoginActivity.class);
+        startActivity(loginIntent);
+        finish();
+    }
+    private void InitializeTransactionList()
+    {
 
         transactionList = new ArrayList<Transaction>();
         for(int i = 0; i < 10 ; i++)
